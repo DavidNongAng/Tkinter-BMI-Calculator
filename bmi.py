962b8d356ec3c50034908fc4543e90061e82fcc5
davidnongang <nongangdavid@gmail.com>
2024-02-08 03:24:02 -0500

    commit #2

--- bmi.py
+++ bmi.py
@@ -10,7 +10,7 @@ class App(ctk.CTk):
     def __init__(self):
         
         # Window Setup
-        super().__init__(fg_color = BLUE)
+        super().__init__(fg_color = GREEN)
         self.title('')
         self.iconbitmap('hidden.ico')
         self.geometry('400x400')
@@ -23,6 +23,7 @@ class App(ctk.CTk):
         
         # Widgets
         ResultText(self)
+        WeightInput(self)
         
         self.mainloop()
     
@@ -40,9 +41,31 @@ class ResultText(ctk.CTkLabel):
     # Constructor
     def __init__(self, parent):
         
-        super().__init__(master = parent, text = 22.5)
+        font = ctk.CTkFont(family = FONT, size = MAIN_TEXT_SIZE, weight = 'bold')
+        super().__init__(master = parent, text = 22.5, font = font, text_color = WHITE)
         self.grid(column = 0, row = 0, rowspan = 2, sticky = 'nsew')
 
+class WeightInput(ctk.CTkFrame):
+    # Constructor
+    def __init__(self, parent):
+        super().__init__(master = parent, fg_color = WHITE)
+        self.grid(column = 0, row = 2, sticky = 'nsew' ,padx = 10, pady = 10)
+
+        # Layout 
+        self.rowconfigure(0, weight = 1, uniform = 'b')
+        self.columnconfigure(1, weight = 2, uniform = 'b')
+        self.columnconfigure(2, weight = 3, uniform = 'b')
+        self.columnconfigure(3, weight = 1, uniform = 'b')
+        self.columnconfigure(4, weight = 2, uniform = 'b')
+        
+        # Widgets
+        font = ctk.CTkFont(family = FONT, size = INPUT_FONT_SIZE)
+        label = ctk.CTkLabel(self, text = '70kg', text_color = BLACK, font = font)
+        label.grid(row = 0, column = 2)
+        
+        # Buttons
+        
+    
 # If we have multiple files, this will be useful
 if __name__ == '__main__':
     App()
